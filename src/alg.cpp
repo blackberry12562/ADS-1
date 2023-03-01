5e927e8148d619cc1b29e7ba40c634848386ebcc
blackberry12562 <116704068+blackberry12562@users.noreply.github.com>
2023-03-01 18:41:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,15 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+int k = 0;
+for (int i = 1; i <= value; i++)
+if (value % i == 0)
+k += 1;
+if (k == 2)
+return true;
+else
+return false;
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -12,9 +18,23 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+if (value <= 1)
+return 2;
+int prime = value;
+bool found = false;
+while (!found) {
+prime++;
+if (checkPrime(prime))
+found = true;
+}
+return prime;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+int sum = 0;
+for (int i = hbound; i >= 2; i--)
+bool isPrime = checkPrime(i);
+if (isPrime)
+sum = sum + i;
+return sum;
 }
